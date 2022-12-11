1c85e4361fe9f9c42e2061e553a759c28a279537
Lucasgomes19 <lucasmadrid01@gmail.com>
2022-12-10 22:39:54 -0300

    Atividade3

---
+++ atv3.java
@@ -0,0 +1,12 @@
+public class atv3 {
+    
+   public static void main(String[] args){
+
+    System.out.println(SomaDosArgumentos(7, 23, 90));
+   }
+    static int SomaDosArgumentos (int a, int b, int c) {
+
+     return a + b +c;
+
+   }
+}
